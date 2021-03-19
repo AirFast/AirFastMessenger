44f542592cbe18fbb923cbaf83bd04d859a3cc33
AirFast <airfast.88@gmail.com>
2021-03-19 09:22:13 +0200

    Add React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+// Pages
+import home from './pages/home';
+import profile from './pages/profile';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-
+        <Switch>
+          <Route exact path={'/'} component={home}/>
+          <Route exact path={'/profile'} component={profile}/>
+          <Route exact path={'/login'} component={login}/>
+          <Route exact path={'/signup'} component={signup}/>
+        </Switch>
     </div>
   );
 }
---
+++ src/pages/profile.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Profile extends Component {
+    render() {
+
+        console.log(this.props)
+
+        return (
+            <div>
+                <h1>Profile page</h1>
+            </div>
+        );
+    }
+}
+
+export default Profile;
